010b1cdb840380120f523448e62ffdf4c342f1d4
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-07-13 22:23:28 +0200

    removed code duplication, fixed device_element_groups vector memory managment, removed method scobe variable declaration and their returns
    Signed-off-by: Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>

--- includes/infomodel/device.hpp
+++ includes/infomodel/device.hpp
@@ -1,33 +1,26 @@
 #ifndef DEVICE_H
 #define DEVICE_H
 
-#include <vector>
 #include <string>
+#include <vector>
 
 #include "deviceElementGroup.hpp"
 #include "namedElement.hpp"
 
-class Device : public NamedElement
-{
+class Device : public NamedElement {
 private:
-	std::vector<DeviceElementGroup*> device_element_groups;
-	
+  std::vector<DeviceElementGroup> device_element_groups;
+
 public:
-	Device(std::string refId, std::string name, std::string desc);
-	
+  Device(std::string refId, std::string name, std::string desc);
 
+  void addDeviceElementGroup(DeviceElementGroup *group);
 
-	void addDeviceElementGroup(DeviceElementGroup * group);
-
-	DeviceElementGroup * getDeviceElementGroup();
-	DeviceElementGroup * getDeviceElementGroup(std::string refId);
-	std::vector<DeviceElementGroup*> getDeviceElementGroups();
-	DeviceElementGroup* findSubElementGroup(DeviceElementGroup* deviceElementGroup, std::string refId);
-	DeviceElement* findSubElement(DeviceElementGroup* deviceElementGroup, std::string refId);
-
-	
+  DeviceElementGroup *getDeviceElementGroup();
+  DeviceElementGroup *getDeviceElementGroup(std::string refId);
+  std::vector<DeviceElementGroup> getDeviceElementGroups();
+  DeviceElement *findSubElement(DeviceElement *deviceElement,
+                                std::string refId);
 };
 
 #endif // !DEVICE_H
-
-
--- sources/infomodel/device.cpp
+++ sources/infomodel/device.cpp
@@ -2,84 +2,51 @@
 
 using namespace std;
 
-Device::Device(string refId, string name, string desc) : NamedElement(refId, name, desc)
-{
-	device_element_groups = vector<DeviceElementGroup*>();
+Device::Device(string refId, string name, string desc)
+    : NamedElement(refId, name, desc) {
+  device_element_groups = vector<DeviceElementGroup>();
 }
 
-
-void Device::addDeviceElementGroup(DeviceElementGroup * group) {
-	this->device_element_groups.push_back(group);
+void Device::addDeviceElementGroup(DeviceElementGroup *group) {
+  device_element_groups.push_back(*group);
 }
 
-DeviceElementGroup * Device::getDeviceElementGroup() {
+DeviceElementGroup *Device::getDeviceElementGroup() {
 
-	auto g1 = this->device_element_groups[0];
-
-	return g1;
-};
-
-DeviceElementGroup * Device::getDeviceElementGroup(string refId) {
-
-	for (int i = 0; i < device_element_groups.size(); i++)
-	{
-		if (device_element_groups[i]->getReferenceId() == refId)
-		{
-			return device_element_groups[i];
-			break;
-		}
-		else if (device_element_groups[i]->getElementType() == Group)
-		{
-			auto group = findSubElementGroup(device_element_groups[i],refId);
-			if (group != nullptr)
-				return group;			
-		}
-		
-	};
-	return nullptr;
-};
-
-DeviceElementGroup* Device::findSubElementGroup(DeviceElementGroup* deviceElementGroup, std::string refId)
-{
-	auto subElementGroups = deviceElementGroup->getSubElementGroups();
-	for (int j = 0; j < subElementGroups.size(); j++)
-	{
-		if (subElementGroups[j]->getReferenceId() == refId)
-		{
-			return subElementGroups[j];
-			break;
-		}
-		else if (subElementGroups[j]->getElementType() == Group)
-		{
-			auto group = findSubElementGroup(subElementGroups[j], refId);
-			if (group != nullptr)
-				return group;
-		}
-	}
-	return nullptr;
+  return &device_element_groups[0];
 }
 
-DeviceElement* Device::findSubElement(DeviceElementGroup* deviceElementGroup, std::string refId)
-{
-	auto subElements = deviceElementGroup->getSubElements();
-	for (int j = 0; j < subElements.size(); j++)
-	{
-		if (subElements[j]->getReferenceId() == refId)
-		{
-			return subElements[j];
-			break;
-		}
-		else if (subElements[j]->getElementType() == Group)
-		{
-			auto group = findSubElement((DeviceElementGroup*) subElements[j], refId);
-			if (group != nullptr)
-				return group;
-		}
-	}
-	return nullptr;
+DeviceElementGroup *Device::getDeviceElementGroup(string refId) {
+
+  for (size_t i = 0; i < device_element_groups.size(); i++) {
+    if (device_element_groups[i].getReferenceId() == refId) {
+      return &device_element_groups[i];
+
+    } else if (device_element_groups[i].getElementType() == Group) {
+      return (DeviceElementGroup *)findSubElement(&device_element_groups[i],
+                                                  refId);
+    }
+  }
+  return nullptr;
 }
 
+DeviceElement *Device::findSubElement(DeviceElement *deviceElement,
+                                      std::string refId) {
+  if (deviceElement->getElementType() == ElementType::Group) {
+    DeviceElementGroup *subGroup = (DeviceElementGroup *)deviceElement;
+    vector<DeviceElement> subElements = subGroup->getSubElements();
+    for (size_t j = 0; j < subElements.size(); j++) {
+      if (subElements[j].getReferenceId() == refId) {
+        return &subElements[j];
+        break;
+      } else if (subElements[j].getElementType() == Group) {
+        return findSubElement(&subElements[j], refId);
+      }
+    }
+  }
+  return nullptr;
+}
 
-vector<DeviceElementGroup *> Device::getDeviceElementGroups() {
-		return device_element_groups;
-};
+vector<DeviceElementGroup> Device::getDeviceElementGroups() {
+  return device_element_groups;
+}
