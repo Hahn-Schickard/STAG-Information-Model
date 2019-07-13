daf58c19969d184668a77335baacc048fa0389d3
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-07-13 22:25:01 +0200

    improved generateRefId method to check for element type, added simple exception handling, removed code duplication
    Signed-off-by: Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>

--- includes/infomodel/blueprint.hpp
+++ includes/infomodel/blueprint.hpp
@@ -1,43 +1,87 @@
 #ifndef BLUEPRINT_H
 #define BLUEPRINT_H
 
-#include <string>
 #include "device.hpp"
 #include "deviceMetric.hpp"
+#include <iostream>
+#include <string>
 
-class BluePrint
-{
+class Blueprint {
 private:
-	Device * device;
-	std::string generateRefId(std::string parentRefId);
+  Device *device;
+  std::string generateRefId(std::string parentRefId, ElementType elementType);
+
 public:
-	
+  virtual ~Blueprint() {}
+  Blueprint(std::string refId, std::string name, std::string desc);
 
-	virtual ~BluePrint() {}
-	BluePrint(std::string refId, std::string name, std::string desc);
-	
-	BluePrint() {
-		this->device = new Device("1234", "TestDevice", "A hardcoded device for testing only");
-		AddDeviceGroup("TestElementGroup", "A hardcoded device element group");
-		auto groupId = device->getDeviceElementGroups()[0]->getReferenceId();
+  Blueprint() {
+    this->device =
+        new Device("1234", "TestDevice", "A hardcoded device for testing only");
 
-		AddSubElement(groupId, "TestObservableMetric", "A hardcoded observableMetric Element", ElementType::Observable, ValueType::ValueDataType::String);
-		AddSubElement(groupId, "TestWriteableMetric", "A hardcoded writeableMetric", ElementType::Writable, ValueType::ValueDataType::Integer);
-		AddSubElement(groupId, "TestDeviceMetric", "A hardcoded deviceMetric", ElementType::Readonly, ValueType::ValueDataType::Float);
-		AddSubElement(groupId, "TestElementGroup2", "A hardcoded device element group", ElementType::Group);
-		auto group = device->getDeviceElementGroup(groupId);
-		auto subGroupId = group->getSubElementGroups()[0]->getReferenceId();
-		AddSubElement(subGroupId, "TestDeviceMetric2", "A hardcoded deviceMetric", ElementType::Readonly, ValueType::ValueDataType::Float);
-	}
+    try {
+      AddDeviceGroup("TestElementGroup", "A hardcoded device element group");
+    } catch (std::exception ex) {
+      std::cout << "Could not add TestElementGroup. " << ex.what() << std::endl;
+    }
 
-	std::string AddDeviceGroup(std::string name, std::string desc);
-	void AddSubElement(std::string groupRefId, std::string name, std::string desc, ElementType elementType) ;
-	void AddSubElement(std::string groupRefId, std::string name, std::string desc, ElementType elementType, ValueType::ValueDataType valueType);
+    const std::string groupId =
+        device->getDeviceElementGroup()->getReferenceId();
 
-	Device * GetDevice(std::string refId) ;
+    try {
+      AddSubElement(groupId, "TestObservableMetric",
+                    "A hardcoded observableMetric Element",
+                    ElementType::Observable, ValueType::ValueDataType::String);
+    } catch (std::exception ex) {
+      std::cout << "Could not add TestObservableMetric. " << ex.what()
+                << std::endl;
+    }
 
-	Device * GetDevice() ;
-	
+    try {
+      AddSubElement(groupId, "TestWriteableMetric",
+                    "A hardcoded writeableMetric", ElementType::Writable,
+                    ValueType::ValueDataType::Integer);
+    } catch (std::exception ex) {
+      std::cout << "Could not add TestWriteableMetric. " << ex.what()
+                << std::endl;
+    }
+
+    try {
+      AddSubElement(groupId, "TestDeviceMetric", "A hardcoded deviceMetric",
+                    ElementType::Readonly, ValueType::ValueDataType::Float);
+    } catch (std::exception ex) {
+      std::cout << "Could not add TestDeviceMetric. " << ex.what() << std::endl;
+    }
+
+    std::string subGroupId;
+    try {
+      subGroupId =
+          AddSubElement(groupId, "TestSubElementGroup1",
+                        "A hardcoded device element group", ElementType::Group);
+    } catch (std::exception ex) {
+      std::cout << "Could not add TestSubElementGroup1. " << ex.what()
+                << std::endl;
+    }
+
+    std::cout << "SubgroupID is set to: " << subGroupId << std::endl;
+    try {
+      AddSubElement(subGroupId, "TestDeviceMetric2",
+                    "A hardcoded deviceMetric ", ElementType::Readonly,
+                    ValueType::ValueDataType::Float);
+    } catch (std::exception ex) {
+      std::cout << "Could not add TestDeviceMetric2. " << ex.what()
+                << std::endl;
+    }
+  }
+
+  std::string AddDeviceGroup(std::string name, std::string desc);
+  std::string AddSubElement(std::string groupRefId, std::string name,
+                            std::string desc, ElementType elementType);
+  std::string AddSubElement(std::string groupRefId, std::string name,
+                            std::string desc, ElementType elementType,
+                            ValueType::ValueDataType valueType);
+
+  Device *GetDevice();
 };
 
 #endif // !BLUEPRINT_H
