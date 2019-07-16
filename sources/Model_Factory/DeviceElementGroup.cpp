2ef75d17d66ca94b13d0dc5f1d092f1bc6eec144
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-07-16 17:18:05 +0200

    added implementation for addDeviceElement()
    Signed-off-by: Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>

--- sources/Model_Factory/DeviceElementGroup.cpp
+++ sources/Model_Factory/DeviceElementGroup.cpp
@@ -1,10 +1,18 @@
 #include "DeviceElementGroup.hpp"
+#include <memory>
 
 using namespace std;
 using namespace Model_Factory;
 
 DeviceElementGroup::DeviceElementGroup(string refId, string name, string desc)
     : DeviceElement(refId, name, desc, ElementType::Group){};
+
+string DeviceElementGroup::addDeviceEelment(const std::string name,
+                                            const std::string desc,
+                                            ElementType type) {
+  iDeviceElement *subelement = new DeviceElement(name, desc, type);
+  subelements.push_back(subelement);
+}
 
 iDeviceElement *DeviceElementGroup::getSubelement(const std::string REF_ID) {
   for (auto subelement : subelements) {
