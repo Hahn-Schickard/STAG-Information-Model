712b15368ca720eaa0d37f2105cfbc37a740676f
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-07-16 17:20:43 +0200

    add Utilities subdir
    Signed-off-by: Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>

---
+++ sources/Utilities/Utils.cpp
@@ -0,0 +1,9 @@
+#include "Utils.hpp"
+
+using namespace std;
+
+std::string generate_Reference_ID(Information_Model::iNamedElement *element) {
+  // @TODO: Implement method stub
+
+  return nullptr;
+}
