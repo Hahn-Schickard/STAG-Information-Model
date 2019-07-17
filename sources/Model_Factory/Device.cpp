cb283805419339c4087523b6e5ace94f5d527c43
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-07-17 18:55:20 +0200

    fixed casting for generate_Reference_ID()

--- sources/Model_Factory/Device.cpp
+++ sources/Model_Factory/Device.cpp
@@ -7,7 +7,7 @@ using namespace Model_Factory;
 string Device::addDeviceElementGroup(const std::string NAME,
                                      const std::string DESC) {
   const string REF_ID = generate_Reference_ID(
-      dynamic_cast<Information_Model::NamedElement *>(this));
+      dynamic_cast<Information_Model::DeviceElement *>(this));
 
   unique_ptr<Information_Model::iDeviceElementGroup> device_element_group_ptr(
       new DeviceElementGroup(REF_ID, NAME, DESC));
