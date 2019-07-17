368ac32c9d844f0ba6850c4b9fdd932f965388e1
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-07-17 17:34:55 +0200

    fixed device impl to work with uinique_ptr

--- sources/Model_Factory/Device.cpp
+++ sources/Model_Factory/Device.cpp
@@ -4,17 +4,19 @@
 using namespace std;
 using namespace Model_Factory;
 
-Device::Device(string refId, string name, string desc)
-    : NamedElement(refId, name, desc), device_element_group(nullptr) {}
-
 string Device::addDeviceElementGroup(const std::string NAME,
                                      const std::string DESC) {
-  string REF_ID = generate_Reference_ID(auto_ptr<iNamedElement>(this));
-  device_element_group = auto_ptr<iDeviceElementGroup>(
-      new DeviceElementGroup((const string)REF_ID, NAME, DESC));
+  const string REF_ID = generate_Reference_ID(
+      dynamic_cast<Information_Model::NamedElement *>(this));
+
+  unique_ptr<Information_Model::iDeviceElementGroup> device_element_group_ptr(
+      new DeviceElementGroup(REF_ID, NAME, DESC));
+
+  device_element_group = move(device_element_group_ptr);
+
   return REF_ID;
 }
 
-iDeviceElementGroup *Device::getDeviceElementGroup() {
-  return device_element_group;
+Information_Model::iDeviceElementGroup *Device::getDeviceElementGroup() {
+  return device_element_group.get();
 }
