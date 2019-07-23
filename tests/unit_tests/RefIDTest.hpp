b0617930ec5730794bf5eee861050770f8bb0327
Gerhard Marki <Gerhard.Marki@Hahn-Schickard.de>
2019-07-23 10:17:05 +0200

    added new tests

--- tests/unit_tests/RefIDTest.hpp
+++ tests/unit_tests/RefIDTest.hpp
@@ -10,89 +10,95 @@
 using namespace Model_Factory;
 using namespace Information_Model;
 
-TEST(RefIdTests, DeviceRefIdIsCorrect) {
-  // Arrange
-  Device *element =
-      new DeviceImpl("1234", "TestDevice", "A hardcoded deviceelement");
+TEST(RefIdTests, DeviceRefIdIsCorrect)
+{
+    // Arrange
+    Device *element =
+        new DeviceImpl("1234", "TestDevice", "A hardcoded deviceelement");
 
-  // Act
-  std::string testedElement = element->getElementRefId();
+    // Act
+    std::string testedElement = element->getElementRefId();
 
-  // Assert
-  std::string expectedResult = "1234";
-  EXPECT_STREQ(expectedResult.c_str(), testedElement.c_str())
-      << "expected: " << expectedResult << std::endl
-      << "provided: " << testedElement << std::endl;
+    // Assert
+    std::string expectedResult = "1234";
+    EXPECT_STREQ(expectedResult.c_str(), testedElement.c_str())
+        << "expected: " << expectedResult << std::endl
+        << "provided: " << testedElement << std::endl;
 }
 
-TEST(RefIdTests, DeviceRefIdIsNotCorrect) {
-  // Arrange
-  Device *element =
-      new DeviceImpl("1234", "TestDevice", "A hardcoded deviceelement");
+TEST(RefIdTests, DeviceRefIdIsNotCorrect)
+{
+    // Arrange
+    Device *element =
+        new DeviceImpl("1234", "TestDevice", "A hardcoded deviceelement");
 
-  // Act
-  std::string testedElement = element->getElementRefId();
-  // Assert
-  std::string expectedResult = "1235";
-  EXPECT_STRNE(expectedResult.c_str(), testedElement.c_str())
-      << "expected: " << expectedResult << std::endl
-      << "provided: " << testedElement << std::endl;
+    // Act
+    std::string testedElement = element->getElementRefId();
+    // Assert
+    std::string expectedResult = "1235";
+    EXPECT_STRNE(expectedResult.c_str(), testedElement.c_str())
+        << "expected: " << expectedResult << std::endl
+        << "provided: " << testedElement << std::endl;
 }
 
-TEST(RefIdTests, DeviceElementRefIdIsCorrect) {
-  // Arrange
-  DeviceElement *element = new DeviceElementBuilder(
-      "1234", "TestDevice", "A hardcoded deviceelement", ElementType::Group);
+TEST(RefIdTests, DeviceElementRefIdIsCorrect)
+{
+    // Arrange
+    DeviceElement *element = new DeviceElementBuilder(
+        "1234", "TestDevice", "A hardcoded deviceelement", ElementType::Group);
 
-  // Act
-  std::string testedElement = element->getElementRefId();
-  // Assert
-  std::string expectedResult = "1234";
-  EXPECT_STREQ(expectedResult.c_str(), testedElement.c_str())
-      << "expected: " << expectedResult << std::endl
-      << "provided: " << testedElement << std::endl;
+    // Act
+    std::string testedElement = element->getElementRefId();
+    // Assert
+    std::string expectedResult = "1234";
+    EXPECT_STREQ(expectedResult.c_str(), testedElement.c_str())
+        << "expected: " << expectedResult << std::endl
+        << "provided: " << testedElement << std::endl;
 }
 
-TEST(RefIdTests, DeviceElementRefIdIsNotCorrect) {
-  // Arrange
-  DeviceElement *element = new DeviceElementBuilder(
-      "1234", "TestDevice", "A hardcoded deviceelement", ElementType::Group);
+TEST(RefIdTests, DeviceElementRefIdIsNotCorrect)
+{
+    // Arrange
+    DeviceElement *element = new DeviceElementBuilder(
+        "1234", "TestDevice", "A hardcoded deviceelement", ElementType::Group);
 
-  // Act
-  std::string testedElement = element->getElementRefId();
-  // Assert
-  std::string expectedResult = "1235";
-  EXPECT_STRNE(expectedResult.c_str(), testedElement.c_str())
-      << "expected: " << expectedResult << std::endl
-      << "provided: " << testedElement << std::endl;
+    // Act
+    std::string testedElement = element->getElementRefId();
+    // Assert
+    std::string expectedResult = "1235";
+    EXPECT_STRNE(expectedResult.c_str(), testedElement.c_str())
+        << "expected: " << expectedResult << std::endl
+        << "provided: " << testedElement << std::endl;
 }
 
-TEST(RefIdTests, DeviceElementGroupRefIdIsCorrect) {
-  // Arrange
-  DeviceElementGroup *element = new DeviceElementGroupImpl(
-      "1234", "TestDevice", "A hardcoded deviceelement");
+TEST(RefIdTests, DeviceElementGroupRefIdIsCorrect)
+{
+    // Arrange
+    DeviceElementGroup *element = new DeviceElementGroupImpl(
+        "1234", "TestDevice", "A hardcoded deviceelement");
 
-  // Act
-  std::string testedElement = element->getElementRefId();
-  // Assert
-  std::string expectedResult = "1234";
-  EXPECT_STREQ(expectedResult.c_str(), testedElement.c_str())
-      << "expected: " << expectedResult << std::endl
-      << "provided: " << testedElement << std::endl;
+    // Act
+    std::string testedElement = element->getElementRefId();
+    // Assert
+    std::string expectedResult = "1234";
+    EXPECT_STREQ(expectedResult.c_str(), testedElement.c_str())
+        << "expected: " << expectedResult << std::endl
+        << "provided: " << testedElement << std::endl;
 }
 
-TEST(RefIdTests, DeviceElementGroupRefIdIsNotCorrect) {
-  // Arrange
-  DeviceElementGroup *element = new DeviceElementGroupImpl(
-      "1234", "TestDevice", "A hardcoded deviceelement");
+TEST(RefIdTests, DeviceElementGroupRefIdIsNotCorrect)
+{
+    // Arrange
+    DeviceElementGroup *element = new DeviceElementGroupImpl(
+        "1234", "TestDevice", "A hardcoded deviceelement");
 
-  // Act
-  std::string testedElement = element->getElementRefId();
-  // Assert
-  std::string expectedResult = "1235";
-  EXPECT_STRNE(expectedResult.c_str(), testedElement.c_str())
-      << "expected: " << expectedResult << std::endl
-      << "provided: " << testedElement << std::endl;
+    // Act
+    std::string testedElement = element->getElementRefId();
+    // Assert
+    std::string expectedResult = "1235";
+    EXPECT_STRNE(expectedResult.c_str(), testedElement.c_str())
+        << "expected: " << expectedResult << std::endl
+        << "provided: " << testedElement << std::endl;
 }
 
 #endif
--- tests/unit_tests/testRunner.cpp
+++ tests/unit_tests/testRunner.cpp
@@ -2,9 +2,11 @@
 #include "ElementNameTest.hpp"
 #include "ElementDescriptionTest.hpp"
 #include "DeviceTest.hpp"
+#include "DeviceElementTest.hpp"
 #include "gtest/gtest.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
