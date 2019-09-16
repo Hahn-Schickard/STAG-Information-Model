97b131dd3cac6dcc72b4ce3a074c9c92d9ad3661
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-09-16 15:54:40 +0200

    reworked macros to use Config.cmake.in files

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -36,43 +36,14 @@ FUNCTION(INCLUDE_DIRS_AND_FILES directory_list included_file_list)
     set(${included_file_list} ${${included_file_list}} PARENT_SCOPE)
 ENDFUNCTION()
 
-FUNCTION(CREATE_PKG_CONFIG_FILE package_name config_file)
-    string(TOLOWER ${package_name} config_file_name)
-    string(CONCAT config_file_name ${config_file_name} "-config.cmake")
-    if(EXISTS ${PROJECT_SOURCE_DIR}/${config_file_name})
-        message(STATUS "Pacakge configuration file: ${PROJECT_SOURCE_DIR}/${config_file_name} already exists")
+FUNCTION(WRITE_PKG_CONFIG_IN_FILE this_project)
+    if(EXISTS ${PROJECT_SOURCE_DIR}/${this_project}Config.cmake.in)
+        message(STATUS "Pacakge input file: ${PROJECT_SOURCE_DIR}/${this_project}Config.cmake.in already exists")
     else()
-        message(STATUS "Creating package configuration file: ${PROJECT_SOURCE_DIR}/${config_file_name}")
-        file(WRITE ${PROJECT_SOURCE_DIR}/${config_file_name}
-            "get_filename_component(SELF_DIR \"\${CMAKE_CURRENT_LIST_FILE}\" PATH)
-include(\${SELF_DIR}/lib/${package_name}.cmake)")
+    message(STATUS "Creating package input file: ${PROJECT_SOURCE_DIR}/${this_project}Config.cmake.in")
+    file(WRITE ${PROJECT_SOURCE_DIR}/${this_project}Config.cmake.in
+        "@PACKAGE_INIT@ 
+include(\"\${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@Targets.cmake\")
+check_required_components(\"@PROJECT_NAME@\")")
     endif()
-    set(${config_file} ${config_file_name} PARENT_SCOPE)
-ENDFUNCTION()
-
-FUNCTION(APPND_ONCE_TO_FILE file_name msg)
-    file(READ ${file_name} file_content)
-    string(FIND ${file_content} ${msg} content_found) 
-    if(${content_found} EQUAL "-1")
-        file(APPEND ${file_name} "
-${msg}")
-    else()
-        message(STATUS "Message: 
-        ${msg}
-already exists in file ${file_name}")
-    endif()
-ENDFUNCTION()
-
-FUNCTION(PROPOGATE_DEPENDENCY_PACKAGE this_projcet package_name)
-    CREATE_PKG_CONFIG_FILE(${this_projcet} pkg_config_file)
-    get_filename_component(dependency_package ${package_name} NAME)
-    string(REGEX MATCH "(-+[0-9][^ ]*)" deletion_mask ${dependency_package})
-    string(REPLACE ${deletion_mask} "" dependency_config_file ${dependency_package})
-    string(TOLOWER ${dependency_config_file} dependency_config_file)
-    message(STATUS "Appending to ${PROJECT_SOURCE_DIR}/${pkg_config_file} dependency: 
-    ${dependency_package}
-with configuration file: 
-    ${dependency_config_file}-config.cmake")
-    APPND_ONCE_TO_FILE("${PROJECT_SOURCE_DIR}/${pkg_config_file}"
-        "include(\${CMAKE_CURRENT_LIST_DIR}/modules/${dependency_package}/${dependency_config_file}-config.cmake)")
 ENDFUNCTION()
