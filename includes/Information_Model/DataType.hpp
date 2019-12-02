b44fb1d28b8b98a8f75e9d109431d9402c7a55f0
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-12-02 14:53:53 +0100

    Implemented DataWrapper class

--- includes/Information_Model/DataType.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef _I_DEVICE_TYPE_HPP
-#define _I_DEVICE_TYPE_HPP
-namespace Information_Model {
-
-enum DataType { Undefined, Integer, String, Boolean, Float };
-
-
-} // namespace Information_Model
-#endif //_I_DEVICE_TYPE_HPP
