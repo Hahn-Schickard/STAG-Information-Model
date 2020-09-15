62796c9bd5450fa48d083ddff5bcaeec5b14f2ae
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-09-15 15:11:34 +0200

    improve integration test

--- conan/test_package/example.cpp
+++ conan/test_package/example.cpp
@@ -1,9 +1,13 @@
-#include "Device.hpp"
+#include "NamedElement.hpp"
 
 #include <iostream>
 
+using namespace std;
 
 int main() {
-
+  auto element = NamedElement("1", "element", "Dummy element");
+  cout << "Element id: " << element.getElementRefId()
+       << " with name: " << element.getElementName()
+       << " described as: " << element.getElementDescription() << endl;
   exit(EXIT_SUCCESS);
 }
