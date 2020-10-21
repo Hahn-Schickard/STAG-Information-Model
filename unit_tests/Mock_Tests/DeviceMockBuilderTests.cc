0adab081de1f8d3f64ddd1a06e131f113a395220
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-10-21 20:23:43 +0200

    add cast to interfaces

--- unit_tests/Mock_Tests/DeviceMockBuilderTests.cc
+++ unit_tests/Mock_Tests/DeviceMockBuilderTests.cc
@@ -14,9 +14,7 @@ TEST(DeviceMockBuilderTests, throwsRuntimeErrorOnNoBase) {
 
   EXPECT_THROW(builder->addDeviceElementGroup("1234", "Mocky", "Mocked device"),
                runtime_error);
-  DevicePtr result;
-  EXPECT_THROW(result = builder->getResult(), runtime_error);
-  result.reset();
+  EXPECT_THROW(builder->getResult(), runtime_error);
 }
 
 TEST(DeviceMockBuilderTests, throwsRuntimeErrorOnOverridingBuild) {
@@ -25,9 +23,7 @@ TEST(DeviceMockBuilderTests, throwsRuntimeErrorOnOverridingBuild) {
   EXPECT_NO_THROW(builder->buildDeviceBase("1234", "Mocky", "Mocked device"));
   EXPECT_THROW(builder->buildDeviceBase("4321", "Schmuck", "Overriding device"),
                runtime_error);
-  DevicePtr result;
-  EXPECT_NO_THROW(result = builder->getResult());
-  result.reset();
+  EXPECT_NO_THROW(builder->getResult());
 }
 
 TEST(DeviceMockBuilderTests, canAddGroup) {
@@ -51,6 +47,7 @@ TEST(DeviceMockBuilderTests, canAddGroup) {
   EXPECT_EQ("Mocky", base_group->getElementName());
   EXPECT_EQ("Mocked device", base_group->getElementDescription());
   EXPECT_EQ(ElementType::GROUP, base_group->getElementType());
+  EXPECT_EQ(1, base_group->getSubelements().size());
 
   auto group_element = base_group->getSubelement(ref_id);
   EXPECT_EQ(ref_id, group_element->getElementId());
@@ -86,6 +83,7 @@ TEST(DeviceMockBuilderTests, canAddSubGroup) {
   EXPECT_EQ("Mocky", base_group->getElementName());
   EXPECT_EQ("Mocked device", base_group->getElementDescription());
   EXPECT_EQ(ElementType::GROUP, base_group->getElementType());
+  EXPECT_EQ(1, base_group->getSubelements().size());
 
   auto group_element = base_group->getSubelement(subgroup_ref_id);
   EXPECT_EQ(subgroup_ref_id, group_element->getElementId());
@@ -117,6 +115,7 @@ TEST(DeviceMockBuilderTests, canAddMetric) {
   EXPECT_EQ("Mocky", base_group->getElementName());
   EXPECT_EQ("Mocked device", base_group->getElementDescription());
   EXPECT_EQ(ElementType::GROUP, base_group->getElementType());
+  EXPECT_EQ(1, base_group->getSubelements().size());
 
   auto element = base_group->getSubelement(ref_id);
   EXPECT_EQ(ref_id, element->getElementId());
@@ -127,10 +126,11 @@ TEST(DeviceMockBuilderTests, canAddMetric) {
   auto mocked_metric = static_pointer_cast<MockMetric>(element);
 
   EXPECT_CALL(*mocked_metric.get(), getDataType());
-  mocked_metric->getDataType();
+  auto metric = static_pointer_cast<Metric>(element);
+  metric->getDataType();
 
   EXPECT_CALL(*mocked_metric.get(), getMetricValue());
-  mocked_metric->getMetricValue();
+  metric->getMetricValue();
 
   EXPECT_THROW(builder->getResult(), runtime_error);
 }
@@ -156,6 +156,7 @@ TEST(DeviceMockBuilderTests, canAddWritableMetric) {
   EXPECT_EQ("Mocky", base_group->getElementName());
   EXPECT_EQ("Mocked device", base_group->getElementDescription());
   EXPECT_EQ(ElementType::GROUP, base_group->getElementType());
+  EXPECT_EQ(1, base_group->getSubelements().size());
 
   auto element = base_group->getSubelement(ref_id);
   EXPECT_EQ(ref_id, element->getElementId());
@@ -166,10 +167,11 @@ TEST(DeviceMockBuilderTests, canAddWritableMetric) {
   auto mocked_metric = static_pointer_cast<MockWritableMetric>(element);
 
   EXPECT_CALL(*mocked_metric.get(), getDataType());
-  mocked_metric->getDataType();
+  auto writable_metric = static_pointer_cast<WritableMetric>(element);
+  writable_metric->getDataType();
 
   EXPECT_CALL(*mocked_metric.get(), getMetricValue());
-  mocked_metric->getMetricValue();
+  writable_metric->getMetricValue();
 
   EXPECT_CALL(*mocked_metric.get(), setMetricValue(::testing::_));
   mocked_metric->setMetricValue((double)20.2);
