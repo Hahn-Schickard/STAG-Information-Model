fbe5b901f679aad1ccfaaeae3717297ed0779d3b
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-10-21 19:44:32 +0200

    add default dtor

--- includes/NamedElement.hpp
+++ includes/NamedElement.hpp
@@ -19,6 +19,8 @@ public:
   const std::string getElementName() const { return name_; }
   const std::string getElementDescription() const { return desc_; }
   const std::string getElementId() const { return refID_; }
+
+  virtual ~NamedElement() = default;
 };
 
 using NamedElementPtr = std::shared_ptr<NamedElement>;
