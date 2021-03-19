82fe01792646317c77492a528b8ae27e57ef256a
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2021-03-19 15:04:17 +0100

    define MOCK_Methods first, change getMetricValue delegation to use lambda

--- includes/mocks/Metric_MOCK.hpp
+++ includes/mocks/Metric_MOCK.hpp
@@ -17,6 +17,7 @@ class MockMetric : public Metric {
 
 public:
   MockMetric() = delete;
+
   MockMetric(const std::string &ref_id, const std::string &name,
              const std::string &desc)
       : Metric(ref_id, name, desc), type_(DataType::UNKNOWN),
@@ -30,20 +31,22 @@ public:
              const std::string &desc, DataType type, const DataVariant &variant)
       : Metric(ref_id, name, desc), type_(type), value_(variant) {}
 
+  MOCK_METHOD(DataVariant, getMetricValue, (), (override));
+  MOCK_METHOD(DataType, getDataType, (), (override));
+
   void delegateToFake() {
     ON_CALL(*this, getMetricValue).WillByDefault(::testing::Return(value_));
     ON_CALL(*this, getDataType).WillByDefault(::testing::Return(type_));
   }
 
   void delegateToFake(std::function<DataVariant()> callback) {
-    ON_CALL(*this, getMetricValue()).WillByDefault(callback);
+    ON_CALL(*this, getMetricValue).WillByDefault([callback]() -> DataVariant {
+      return callback();
+    });
     ON_CALL(*this, getDataType).WillByDefault(::testing::Return(type_));
   }
 
   ~MockMetric() { ::testing::Mock::VerifyAndClear(this); }
-
-  MOCK_METHOD(DataVariant, getMetricValue, (), (override));
-  MOCK_METHOD(DataType, getDataType, (), (override));
 };
 
 using MockMetricPtr = std::shared_ptr<MockMetric>;
