61ec30228b0bf9b1b150c0dbd3b4cbfc8eca9106
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2021-03-19 15:04:37 +0100

    add delegation to fake

--- includes/mocks/WritableMetric_MOCK.hpp
+++ includes/mocks/WritableMetric_MOCK.hpp
@@ -13,14 +13,45 @@ namespace testing {
  *
  */
 class MockWritableMetric : public WritableMetric {
+  DataType type_;
+  DataVariant value_;
+
 public:
+  MockWritableMetric() = delete;
+
   MockWritableMetric(const std::string &ref_id, const std::string &name,
                      const std::string &desc)
-      : WritableMetric(ref_id, name, desc) {}
+      : WritableMetric(ref_id, name, desc), type_(DataType::UNKNOWN),
+        value_(DataVariant((bool)false)) {}
+
+  MockWritableMetric(const std::string &ref_id, const std::string &name,
+                     const std::string &desc, DataType type)
+      : WritableMetric(ref_id, name, desc), type_(type),
+        value_(setVariant(type_)) {}
+
+  MockWritableMetric(const std::string &ref_id, const std::string &name,
+                     const std::string &desc, DataType type,
+                     const DataVariant &variant)
+      : WritableMetric(ref_id, name, desc), type_(type), value_(variant) {}
 
   MOCK_METHOD(DataVariant, getMetricValue, (), (override));
   MOCK_METHOD(void, setMetricValue, (DataVariant), (override));
   MOCK_METHOD(DataType, getDataType, (), (override));
+
+  void delegateToFake() {
+    ON_CALL(*this, getMetricValue).WillByDefault(::testing::Return(value_));
+    ON_CALL(*this, getDataType).WillByDefault(::testing::Return(type_));
+  }
+
+  void delegateToFake(std::function<DataVariant()> reader,
+                      std::function<void(DataVariant)> writer) {
+    ON_CALL(*this, getMetricValue).WillByDefault(reader);
+    ON_CALL(*this, setMetricValue)
+        .WillByDefault([this, writer](DataVariant value) { writer(value); });
+    ON_CALL(*this, getDataType).WillByDefault(::testing::Return(type_));
+  }
+
+  ~MockWritableMetric() { ::testing::Mock::VerifyAndClear(this); }
 };
 
 using MockWritableMetricPtr = std::shared_ptr<MockWritableMetric>;
