ea1076bde02486bb0610d5d93b39b485f8d4653f
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-11-17 14:12:51 +0100

    add exception handling to mock test

--- sources/runner/main.cpp
+++ sources/runner/main.cpp
@@ -15,26 +15,34 @@ void print(NonemptyMetricPtr element, size_t offset);
 void print(NonemptyDeviceElementGroupPtr elements, size_t offset);
 
 int main() {
-  DevicePtr device;
-  {
-    auto builder = new Information_Model::testing::DeviceMockBuilder();
-    builder->buildDeviceBase("9876", "Mocky", "Mocked test device");
-    auto subgroup_1_ref_id =
-        builder->addDeviceElementGroup("Group 1", "First group");
-    auto boolean_ref_id = builder->addReadableMetric(subgroup_1_ref_id,
-        "Boolean", "Mocked readable metric", DataType::BOOLEAN);
-    auto integer_ref_id = builder->addReadableMetric(
-        "Integer", "Mocked readable metric", DataType::INTEGER);
-    auto string_ref_id = builder->addReadableMetric(
-        "String", "Mocked readable metric", DataType::STRING);
+  try {
+    DevicePtr device;
+    {
+      auto* builder = new Information_Model::testing::DeviceMockBuilder();
+      builder->buildDeviceBase("9876", "Mocky", "Mocked test device");
+      auto subgroup_1_ref_id =
+          builder->addDeviceElementGroup("Group 1", "First group");
+      auto boolean_ref_id = builder->addReadableMetric(subgroup_1_ref_id,
+          "Boolean",
+          "Mocked readable metric",
+          DataType::BOOLEAN);
+      auto integer_ref_id = builder->addReadableMetric(
+          "Integer", "Mocked readable metric", DataType::INTEGER);
+      auto string_ref_id = builder->addReadableMetric(
+          "String", "Mocked readable metric", DataType::STRING);
 
-    device = builder->getResult();
-    delete builder;
+      device = builder->getResult();
+      delete builder;
+    }
+
+    print(device);
+
+    return EXIT_SUCCESS;
+  } catch (const exception& ex) {
+    cerr << "An unhandled exception occurred during mock test. Exception: "
+         << ex.what() << endl;
+    return EXIT_FAILURE;
   }
-
-  print(device);
-
-  return EXIT_SUCCESS;
 }
 
 void print(NonemptyDeviceElementGroupPtr elements, size_t offset) {
