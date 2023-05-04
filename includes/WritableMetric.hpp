aba6747e4bcaa28e65f1a6759c876a1b21709329
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2023-05-04 12:00:09 +0200

    refactor WritableMetric into a struct, inherit from Metric

--- includes/WritableMetric.hpp
+++ includes/WritableMetric.hpp
@@ -1,13 +1,7 @@
 #ifndef __INFORMATION_MODEL_WRITEABLE_METRIC_HPP
 #define __INFORMATION_MODEL_WRITEABLE_METRIC_HPP
 
-#include "DataVariant.hpp"
-
-#include <memory>
-#include <stdexcept>
-#include <string>
-
-#include "Nonempty_Pointer/NonemptyPtr.hpp"
+#include "Metric.hpp"
 
 namespace Information_Model {
 /**
@@ -16,27 +10,16 @@ namespace Information_Model {
  * default data type value (for example: bool = true, int = 0, string = "");
  *
  */
-class WritableMetric {
-protected:
-  WritableMetric() = default;
-
-public:
-  virtual DataVariant getMetricValue() {
-    throw std::runtime_error(
-        "Called based implementation of WritableMetric::getMetricValue()");
-  }
-
+struct WritableMetric : public Metric {
   virtual void setMetricValue(DataVariant /*value*/) {
     throw std::runtime_error(
         "Called based implementation of WritableMetric::setMetricValue()");
   }
 
-  virtual DataType getDataType() {
-    throw std::runtime_error(
-        "Called based implementation of WritableMetric::getDataType()");
-  }
-
   virtual ~WritableMetric() = default;
+
+protected:
+  WritableMetric() = default;
 };
 
 using WritableMetricPtr = std::shared_ptr<WritableMetric>;
