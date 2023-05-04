91d56d849cdd98bfe92df36bc30bb90e8b4f310f
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2023-05-04 19:05:53 +0200

    add draft Function model

---
+++ includes/Function.hpp
@@ -0,0 +1,234 @@
+#ifndef __INFORMATION_MODEL_FUNCTION_HPP
+#define __INFORMATION_MODEL_FUNCTION_HPP
+
+#include "DataVariant.hpp"
+
+#include <future>
+#include <memory>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+
+#include "Nonempty_Pointer/NonemptyPtr.hpp"
+
+namespace Information_Model {
+/**
+ * @addtogroup FunctionModeling Function Modelling
+ * @{
+ */
+/**
+ * @brief An interface to a Function.
+ *
+ * Models a single functionality for various sensors/actors
+ *
+ * @attention
+ * This interface is implemented in Information Model Manager Project and is
+ * built via DeviceBuilderInterface::addFunction() or
+ * DeviceBuilderInterface::addDeviceElement() with type set to
+ * ElementType::FUNCTION
+ */
+
+struct Function {
+  using Parameter = std::optional<DataVariant>;
+  /**
+   * @brief Indexed map of the modeled function parameters
+   *
+   * @param Key - parameter number in the function
+   * @param Value - parameter value
+   */
+  using Parameters = std::unordered_map<uintmax_t, Parameter>;
+
+  /**
+   * @brief Contains parameter type information
+   *
+   * @param First - parameter data type
+   * @param Second - indicates if this parameter is optional
+   *
+   */
+  using ParameterType = std::pair<DataType, bool>;
+
+  /**
+   * @brief Indexed map of the modeled function parameter types
+   *
+   * @param Key - parameter number in the function
+   * @param Value - parameter type
+   */
+  using ParameterTypes = std::unordered_map<uintmax_t, ParameterType>;
+
+  /**
+   * @brief Async response buffer
+   *
+   * @param First - async call id
+   * @param Second - result buffer
+   */
+  using ResultFuture = std::pair<uintmax_t, std::future<DataVariant>>;
+
+  /**
+   * @brief Executes the modeled functionality without waiting for the execution
+   * result
+   *
+   * @param parameters
+   */
+  virtual void execute(Parameters /*parameters*/ = Parameters()) {
+    throw std::runtime_error(
+        "Called based implementation of Function::execute()");
+  }
+
+  /**
+   * @brief Calls the modeled functionality and waits to return the execution
+   * result
+   *
+   * Blocks until the execution result is available or a timeout occurs
+   *
+   * @throws std::runtime_error - if modeled functionality does not support
+   * returning execution result
+   *
+   * @param parameters
+   * @param timeout - number of miliseconds until a timeout occurs
+   * @return DataVariant
+   */
+  virtual DataVariant call(
+      uintmax_t /*timeout*/ = 100, Parameters /*parameters*/ = Parameters()) {
+    throw std::runtime_error("Function does not support returning results");
+  }
+
+  /**
+   * @brief Calls the modeled functionality and allocates a future for the
+   * execution result
+   *
+   * Blocks until the execution call is dispatched
+   *
+   * @throws std::runtime_error - if modeled functionality does not support
+   * returning execution result
+   *
+   * @param parameters
+   * @return ResultFuture
+   */
+  virtual ResultFuture asyncCall(Parameters /*parameters*/ = Parameters()) {
+    throw std::runtime_error("Function does not support returning results");
+  }
+
+  /**
+   * @brief Cancels a given asynchronous call
+   *
+   * The linked future result (the second Function::ResultFuture parameter) will
+   * throw an exception to indicate that it was canceled
+   *
+   * @throws std::runtime_error - if the given call_id does not indicate a
+   * previous asynchronous call
+   *
+   * @param call_id - obtained from the first ResultFuture parameter
+   */
+  virtual void cancelAsyncCall(uintmax_t /*call_id*/) {
+    throw std::runtime_error(
+        "Called based implementation of Function::cancelAsyncCall()");
+  }
+
+  /**
+   * @brief Returns the supported result type
+   *
+   * If the modeled Function does not support returning a result, this method
+   * will return DataType::UNKNOWN
+   *
+   * @return DataType
+   */
+  virtual DataType getResultDataType() {
+    throw std::runtime_error(
+        "Called based implementation of Function::getResultDataType()");
+  }
+
+  /**
+   * @brief Returns an index map of supported Parameter types
+
+   *
+   * @return ParameterTypes
+   */
+  virtual ParameterTypes getSupportedParameterTypes() {
+    throw std::runtime_error("Called based implementation of "
+                             "Function::getSupportedParameterTypes()");
+  }
+
+protected:
+  Function() = default;
+}; // namespace Information_Model
+
+/**
+ * @brief Helper function to expand an existing parameter map with a given
+ * Function::Parameter
+ *
+ * @throws std::range_error - if Function::Parameter value is already assigned
+ * at a given parameter number
+ *
+ * @param map
+ * @param param_number
+ * @param param
+ */
+inline void addParameter(Function::Parameters& map,
+    uintmax_t param_number,
+    Function::Parameter param) {
+  if (!map.try_emplace(param_number, param).second) {
+    auto existing = map[param_number].value_or(DataVariant());
+    auto param_value = param.value_or(DataVariant());
+    std::string error_msg = "Could not add " +
+        toString(toDataType(param_value)) + "(" + toString(param_value) + ")" +
+        " as parameter number " + std::to_string(param_number) +
+        " because parameter " + toString(toDataType(existing)) + "(" +
+        toString(existing) + ") is already assigned to that number";
+    throw std::range_error(error_msg);
+  }
+}
+
+/**
+ * @brief Helper function to expand an existing parameter map with supported
+ * parameter types only
+ *
+ * @throws std::invalid_argument - if parameter type or number is not supported
+ * @throws std::range_error - if Function::Parameter value is already assigned
+ * at a given parameter number
+ *
+ * @param map
+ * @param supported_types
+ * @param param_number
+ * @param param
+ */
+inline void addSupportedParameter(Function::Parameters& map,
+    const Function::ParameterTypes& supported_types,
+    uintmax_t param_number,
+    Function::Parameter param) {
+  auto supported_types_iter = supported_types.find(param_number);
+  if (supported_types_iter != supported_types.end()) {
+    auto supported_type = supported_types_iter->second;
+    if (param.has_value()) {
+      auto param_value = param.value();
+      if (matchVariantType(param_value, supported_type.first)) {
+        addParameter(map, param_number, param);
+      } else {
+        std::string error_msg = "Parameter number " +
+            std::to_string(param_number) + " " +
+            toString(supported_type.first) + " does not support " +
+            toString(toDataType(param_value)) + "(" + toString(param_value) +
+            ") value";
+        throw std::invalid_argument(error_msg);
+      }
+    } else if (supported_type.second) {
+      addParameter(map, param_number, std::nullopt);
+    } else {
+      std::string error_msg = "Parameter number " +
+          std::to_string(param_number) + " " + toString(supported_type.first) +
+          " does not support empty values";
+      throw std::invalid_argument(error_msg);
+    }
+  } else {
+    std::string error_msg = "Parameter number " + std::to_string(param_number) +
+        " is not supported";
+    throw std::invalid_argument(error_msg);
+  }
+}
+
+using FunctionPtr = std::shared_ptr<Function>;
+using NonemptyFunctionPtr = NonemptyPointer::NonemptyPtr<FunctionPtr>;
+
+/** @}*/
+} // namespace Information_Model
+#endif //__INFORMATION_MODEL_FUNCTION_HPP
