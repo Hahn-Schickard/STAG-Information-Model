2d3ce54020b4bfb36b04b34a948e96c6347efedd
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2023-05-11 13:41:24 +0200

    move execute callbacks into the fake

--- unit_tests/Mock_Tests/FunctionTests.cc
+++ unit_tests/Mock_Tests/FunctionTests.cc
@@ -248,7 +248,7 @@ protected:
     MockFunction::Canceler cancel_cb = [this](uintmax_t call_id) {
       executor->cancel(call_id);
     };
-    function_mock->delegateToFake(execute_cb, cancel_cb);
+    function_mock->delegateToFake(move(execute_cb), move(cancel_cb));
   }
 
   FunctionExpectationsPtr expectations;
