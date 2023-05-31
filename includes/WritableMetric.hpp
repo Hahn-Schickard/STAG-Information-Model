7222aeb3e34f04458204b1b0f7061dd24a322120
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2023-05-31 18:18:11 +0200

    fix WritableMetric documentation

--- includes/WritableMetric.hpp
+++ includes/WritableMetric.hpp
@@ -5,22 +5,17 @@
 
 namespace Information_Model {
 /**
- * @brief A read and writable metric. If this metric does not support active
- * value reading (Readable part), getMetricValue() calls will throw a generic
- * std::logic_error exception
+ * @brief A read and writable metric.
  *
  */
 struct WritableMetric : public Metric {
-  DataVariant getMetricValue() override {
-    throw std::logic_error("This metric does not support read functionality");
-  }
-
   /**
    * @brief Writes the given DataVariant as a metric value to the modeled
    * sensor/actor system
    *
    * @throws std::invalid_argument if provided argument does not match the
    * modeled value type
+   * @throws std::logic_error if internal setter callback does not exist
    *
    */
   virtual void setMetricValue(DataVariant /*value*/) {
@@ -31,8 +26,7 @@ struct WritableMetric : public Metric {
   /**
    * @brief Checks if the modeled metric does not supports value reading
    *
-   * @return true
-   * @return false
+   * @return bool
    */
   virtual bool isWriteOnly() {
     throw std::runtime_error(
