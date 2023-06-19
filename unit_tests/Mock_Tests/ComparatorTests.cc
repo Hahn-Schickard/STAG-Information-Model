5a1d9b74a308f8ae40dc6a2087e5a6addfbdb629
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2023-06-19 14:55:56 +0200

    add emptyGroupsAreEqual test case

--- unit_tests/Mock_Tests/ComparatorTests.cc
+++ unit_tests/Mock_Tests/ComparatorTests.cc
@@ -338,3 +338,17 @@ TYPED_TEST(Comparator_TestSuite, isNotEqual) {
       << this->param.name
       << " comparator tested value is the same as another value";
 }
+
+TEST(DeviceElementGroupComparisonTests, emptyGroupsAreEqual) {
+  auto builder = TestElementBuilder();
+  auto empty_group_info = make_shared<TestElementInfo>(
+      ElementMetaInfo("", "empty-group", "Just an empty group mock"));
+  auto first_group = builder.build<DeviceElementGroup>(empty_group_info);
+  builder.resetBuilder();
+  auto second_group = builder.build<DeviceElementGroup>(empty_group_info);
+
+  EXPECT_NE(first_group, second_group)
+      << "First group points to the same pointer as second group";
+  EXPECT_EQ(*first_group, *second_group)
+      << "First group is not equal to the second one";
+}
