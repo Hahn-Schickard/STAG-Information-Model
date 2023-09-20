778cce8ec03fca165360d6d6341dd184946b28ee
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2023-09-20 12:53:10 +0200

    add test cases for default actions, refactor setNewVariant into ParameterizedWritableMetricTests::getNewVariant(), change EXPECT_CALL times expectation to be exactly once

--- unit_tests/Mock_Tests/WritableMetricTests.cc
+++ unit_tests/Mock_Tests/WritableMetricTests.cc
@@ -7,84 +7,139 @@
 using namespace Information_Model;
 using namespace Information_Model::testing;
 using namespace std;
-using ::testing::AtLeast;
+using ::testing::Exactly;
 
 class ParameterizedWritableMetricTests
     : public ::testing::TestWithParam<DataType> {
   DataVariant value_;
 
-  DataVariant read() { return value_; }
+  DataVariant read() const { return value_; }
 
   void write(DataVariant value) { value_ = move(value); }
 
 protected:
   void SetUp() override {
     type = GetParam();
-    value_ = setVariant(type).value();
-    writable = make_shared<MockWritableMetric>(type);
+    value_ = getNewVariant();
+    tested = make_shared<MockWritableMetric>(type);
+  }
+
+  void bindActions() {
     auto reader = bind(&ParameterizedWritableMetricTests::read, this);
     auto writer =
         bind(&ParameterizedWritableMetricTests::write, this, placeholders::_1);
-    writable->delegateToFake(writer, reader);
+    tested->delegateToFake(writer, reader);
   }
+
+  void unbindActions() { tested->delegateToFake(nullptr, nullptr); }
+
+  DataVariant getNewVariant() const {
+    switch (type) {
+    // NOLINTBEGIN(readability-magic-numbers)
+    case DataType::BOOLEAN:
+      return DataVariant((bool)true);
+    case DataType::INTEGER:
+      return DataVariant((intmax_t)-10);
+    case DataType::UNSIGNED_INTEGER:
+      return DataVariant((uintmax_t)10);
+    case DataType::DOUBLE:
+      return DataVariant((double)20.0);
+    case DataType::TIME:
+      return DataVariant(DateTime(25));
+    case DataType::OPAQUE:
+      return DataVariant(vector<uint8_t>{1, 2, 3, 4, 5});
+    case DataType::STRING:
+      return DataVariant(string("Hello World!"));
+    case DataType::NONE:
+    case DataType::UNKNOWN:
+    // NOLINTEND(readability-magic-numbers)
+    default:
+      throw logic_error(
+          "Can not initialise variant value with unknown or none data types");
+    }
+  }
+
+  DataVariant getReadValue() const { return value_; }
 
   DataType type; // NOLINT(readability-identifier-naming)
-  MockWritableMetricPtr writable; // NOLINT(readability-identifier-naming)
+  MockWritableMetricPtr tested; // NOLINT(readability-identifier-naming)
 };
 
-TEST_P(ParameterizedWritableMetricTests, canRead) {
-  EXPECT_CALL(*writable, getMetricValue()).Times(AtLeast(1));
-  EXPECT_NO_THROW(writable->getMetricValue());
-}
-
-TEST_P(ParameterizedWritableMetricTests, canWrite) {
-  EXPECT_CALL(*writable, setMetricValue(::testing::_)).Times(AtLeast(1));
-  EXPECT_NO_THROW(writable->setMetricValue(setVariant(type).value()));
-}
-
-inline DataVariant setNewVariant(DataType type) {
-  switch (type) {
-  // NOLINTBEGIN(readability-magic-numbers)
-  case DataType::BOOLEAN:
-    return DataVariant((bool)true);
-  case DataType::INTEGER:
-    return DataVariant((intmax_t)-10);
-  case DataType::UNSIGNED_INTEGER:
-    return DataVariant((uintmax_t)10);
-  case DataType::DOUBLE:
-    return DataVariant((double)20.0);
-  case DataType::TIME:
-    return DataVariant(DateTime(25));
-  case DataType::OPAQUE:
-    return DataVariant(vector<uint8_t>{1, 2, 3, 4, 5});
-  case DataType::STRING:
-    return DataVariant(string("Hello World!"));
-  case DataType::NONE:
-  case DataType::UNKNOWN:
-  // NOLINTEND(readability-magic-numbers)
-  default:
-    throw logic_error(
-        "Can not initialise variant value with unknown or none data types");
-  }
-}
-
-TEST_P(ParameterizedWritableMetricTests, canReadWrittenValue) {
-  EXPECT_CALL(*writable, setMetricValue(::testing::_)).Times(AtLeast(1));
-
-  auto new_value = setNewVariant(type);
-  EXPECT_NO_THROW(writable->setMetricValue(new_value));
-
-  EXPECT_CALL(*writable, getMetricValue()).Times(AtLeast(1));
+TEST_P(ParameterizedWritableMetricTests, canReadDefault) {
+  EXPECT_CALL(*tested, getMetricValue()).Times(Exactly(1));
 
   optional<DataVariant> read_value = nullopt;
   EXPECT_FALSE(read_value.has_value());
-  EXPECT_NO_THROW(read_value = writable->getMetricValue());
+  EXPECT_NO_THROW(read_value = tested->getMetricValue());
+  EXPECT_TRUE(read_value.has_value());
+
+  auto default_value = setVariant(type).value();
+  EXPECT_EQ(default_value, read_value.value());
+}
+
+TEST_P(ParameterizedWritableMetricTests, canReadCustom) {
+  EXPECT_CALL(*tested, getMetricValue()).Times(Exactly(1));
+  bindActions();
+
+  optional<DataVariant> read_value = nullopt;
+  EXPECT_FALSE(read_value.has_value());
+  EXPECT_NO_THROW(read_value = tested->getMetricValue());
+  EXPECT_TRUE(read_value.has_value());
+
+  EXPECT_EQ(getReadValue(), read_value.value());
+}
+
+TEST_P(ParameterizedWritableMetricTests, canResetRead) {
+  EXPECT_CALL(*tested, getMetricValue()).Times(Exactly(1));
+  bindActions();
+
+  optional<DataVariant> read_value = nullopt;
+  EXPECT_FALSE(read_value.has_value());
+  EXPECT_NO_THROW(read_value = tested->getMetricValue());
+  EXPECT_TRUE(read_value.has_value());
+
+  EXPECT_EQ(getReadValue(), read_value.value());
+  EXPECT_CALL(*tested, getMetricValue()).Times(Exactly(1));
+  unbindActions();
+
+  read_value = nullopt;
+  EXPECT_FALSE(read_value.has_value());
+  EXPECT_NO_THROW(read_value = tested->getMetricValue());
+  EXPECT_TRUE(read_value.has_value());
+
+  auto default_value = setVariant(type).value();
+  EXPECT_EQ(default_value, read_value.value());
+  EXPECT_EQ(getNewVariant(), getReadValue());
+}
+
+TEST_P(ParameterizedWritableMetricTests, canWriteCustom) {
+  EXPECT_CALL(*tested, setMetricValue(::testing::_)).Times(Exactly(1));
+  bindActions();
+
+  auto default_value = setVariant(type).value();
+  EXPECT_NO_THROW(tested->setMetricValue(default_value));
+
+  EXPECT_EQ(default_value, getReadValue());
+}
+
+TEST_P(ParameterizedWritableMetricTests, canReadWrittenValue) {
+  EXPECT_CALL(*tested, setMetricValue(::testing::_)).Times(Exactly(1));
+  bindActions();
+
+  auto new_value = getNewVariant();
+  EXPECT_NO_THROW(tested->setMetricValue(new_value));
+
+  EXPECT_CALL(*tested, getMetricValue()).Times(Exactly(1));
+
+  optional<DataVariant> read_value = nullopt;
+  EXPECT_FALSE(read_value.has_value());
+  EXPECT_NO_THROW(read_value = tested->getMetricValue());
   EXPECT_TRUE(read_value.has_value());
   EXPECT_EQ(new_value, read_value.value());
 }
 
 TEST_P(ParameterizedWritableMetricTests, canGetType) {
-  EXPECT_EQ(type, writable->getDataType());
+  EXPECT_EQ(type, tested->getDataType());
 }
 
 struct SetWritableMetricTestNameSuffix {
