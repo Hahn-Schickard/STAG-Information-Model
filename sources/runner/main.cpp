880d5868439d572eb15e6488eb12d1d7ed1b395a
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2025-01-15 15:24:12 +0100

    add custom Executor example

--- sources/runner/main.cpp
+++ sources/runner/main.cpp
@@ -5,6 +5,7 @@
 #include "WritableMetric.hpp"
 
 #include <iostream>
+#include <thread>
 
 using namespace std;
 using namespace Information_Model;
@@ -16,12 +17,72 @@ void print(const NonemptyMetricPtr& element, size_t offset);
 void print(const NonemptyFunctionPtr& element, size_t offset);
 void print(const NonemptyDeviceElementGroupPtr& elements, size_t offset);
 
+struct Executor {
+  using Callback = function<void(void)>;
+  Executor() : callback_([]() { cout << "Callback called" << endl; }) {}
+
+  pair<uintmax_t, future<DataVariant>> execute(const Function::Parameters&) {
+    auto execute_lock = lock_guard(execute_mx_);
+    auto id = calls_.size();
+    auto promise = std::promise<DataVariant>();
+    auto future = make_pair(id, promise.get_future());
+    calls_.emplace(id, move(promise));
+    return future;
+  }
+
+  void cancel(uintmax_t id) {
+    auto iter = calls_.find(id);
+    if (iter != calls_.end()) {
+      iter->second.set_exception(
+          make_exception_ptr(CallCanceled(id, "ExternalExecutor")));
+      auto clear_lock = lock_guard(erase_mx_);
+      iter = calls_.erase(iter);
+    } else {
+      throw CallerNotFound(id, "ExternalExecutor");
+    }
+  }
+
+  void respondAll() {
+    auto execute_lock = lock_guard(execute_mx_);
+    auto erase_lock = lock_guard(erase_mx_);
+    // NOLINTNEXTLINE(modernize-loop-convert)
+    for (auto it = calls_.begin(); it != calls_.end(); it++) {
+      callback_();
+      it->second.set_value(DataVariant());
+    }
+    calls_.clear();
+  }
+
+private:
+  void respond(uintmax_t id) {
+    auto erase_lock = lock_guard(erase_mx_);
+    auto it = calls_.find(id);
+    if (it != calls_.end()) {
+      if (callback_) {
+        callback_();
+        it->second.set_value(DataVariant());
+      } else {
+        it->second.set_exception(
+            make_exception_ptr(runtime_error("Callback dos not exist")));
+      }
+      it = calls_.erase(it);
+    }
+  }
+
+  Callback callback_;
+  mutex execute_mx_;
+  mutex erase_mx_;
+  unordered_map<uintmax_t, promise<DataVariant>> calls_;
+};
+
 int main() {
   try {
+    auto executor = make_shared<Executor>();
     DevicePtr device;
     string readable_id;
     string callable_id;
     string executable_id;
+    string custom_executable_id;
     {
       auto* builder = new Information_Model::testing::DeviceMockBuilder();
       builder->buildDeviceBase("9876", "Mocky", "Mocked test device");
@@ -40,6 +101,12 @@ int main() {
           "ReturnsBoolean", "Mocked function with return", DataType::BOOLEAN);
       executable_id = builder->addFunction(
           "ReturnsNone", "Mocked function with no return", DataType::NONE);
+      custom_executable_id = builder->addFunction("CustomExecutable",
+          "Mocked function with custom executor that does not return any "
+          "values",
+          DataType::NONE,
+          bind(&Executor::execute, executor, placeholders::_1),
+          bind(&Executor::cancel, executor, placeholders::_1));
 
       device = move(builder->getResult());
       delete builder;
@@ -72,7 +139,8 @@ int main() {
 
     auto custom_executable = get<NonemptyFunctionPtr>(
         device->getDeviceElement(custom_executable_id)->functionality);
-    executable->execute();
+    custom_executable->execute();
+    executor->respondAll();
 
     return EXIT_SUCCESS;
   } catch (const exception& ex) {
