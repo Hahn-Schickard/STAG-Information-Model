9b3813724f5acd445cc2f36619c8532ee14e9757
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2025-01-15 16:01:19 +0100

    refactor integration test

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -6,107 +6,55 @@
 
 #include <exception>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 using namespace Information_Model;
-
-void print(const DevicePtr& device);
-void print(NonemptyDeviceElementPtr element, size_t offset);
-void print(NonemptyWritableMetricPtr element, size_t offset);
-void print(NonemptyMetricPtr element, size_t offset);
-void print(NonemptyFunctionPtr element, size_t offset);
-void print(NonemptyDeviceElementGroupPtr elements, size_t offset);
+using namespace Information_Model::testing;
 
 int main() {
   try {
     DevicePtr device;
-    string read_target_id;
+    vector<string> ids;
     {
-      auto* builder = new Information_Model::testing::DeviceMockBuilder();
+      auto builder = make_shared<DeviceMockBuilder>();
       builder->buildDeviceBase("9876", "Mocky", "Mocked test device");
-      auto subgroup_1_ref_id =
-          builder->addDeviceElementGroup("Group 1", "First group");
-      auto boolean_ref_id = builder->addReadableMetric(subgroup_1_ref_id,
-          "Boolean",
-          "Mocked readable metric",
-          DataType::BOOLEAN);
-      auto integer_ref_id = builder->addReadableMetric(
-          "Integer", "Mocked readable metric", DataType::INTEGER);
-      read_target_id = integer_ref_id;
-      builder->addWritableMetric(
-          "String", "Mocked writable metric", DataType::STRING);
-      builder->addFunction("Boolean", "Mocked function", DataType::BOOLEAN);
+      ids.push_back(builder->addDeviceElementGroup("Group 1", "First group"));
+      ids.push_back(builder->addReadableMetric(
+          ids[0], "Status", "Mocked readable metric", DataType::BOOLEAN));
+      ids.push_back(builder->addReadableMetric(
+          "Temperature", "Mocked readable metric", DataType::INTEGER));
+      ids.push_back(builder->addWritableMetric(
+          "Label", "Mocked writable metric", DataType::STRING));
+      ids.push_back(builder->addFunction(
+          "Multiplicator", "Mocked function", DataType::DOUBLE));
 
       device = move(builder->getResult());
-      delete builder;
     }
 
-    print(device);
+    for (const auto& id : ids) {
+      auto element = device->getDeviceElement(id);
+      match(
+          element->functionality,
+          [id](const NonemptyDeviceElementGroupPtr&) {
+            cout << "Element " + id + " is a group" << endl;
+          },
+          [id](const NonemptyMetricPtr&) {
+            cout << "Element " + id + " is a readable metric" << endl;
+          },
+          [id](const NonemptyWritableMetricPtr&) {
+            cout << "Element " + id + " is a writable metric" << endl;
+          },
+          [id](const NonemptyFunctionPtr&) {
+            cout << "Element " + id + " is a function" << endl;
+          });
+    }
 
-    auto read_target_metric = get<NonemptyMetricPtr>(
-        device->getDeviceElement(read_target_id)->functionality);
-    auto value = get<intmax_t>(read_target_metric->getMetricValue());
-
-    cout << "Reading " << read_target_id << " metric value as " << value
-         << endl;
-
+    cout << "Integration test successful" << endl;
     return EXIT_SUCCESS;
   } catch (const exception& ex) {
     cerr << "An unhandled exception occurred during mock test. Exception: "
          << ex.what() << endl;
     return EXIT_FAILURE;
   }
-}
-
-void print(NonemptyDeviceElementGroupPtr elements, size_t offset) {
-  cout << string(offset, ' ') << "Group contains elements:" << endl;
-  for (auto element : elements->getSubelements()) {
-    print(element, offset + 3);
-  }
-}
-
-void print(NonemptyMetricPtr element, size_t offset) {
-  cout << string(offset, ' ') << "Reads " << toString(element->getDataType())
-       << " value: " << toString(element->getMetricValue()) << endl;
-  cout << endl;
-}
-
-void print(NonemptyWritableMetricPtr element, size_t offset) {
-  cout << string(offset, ' ') << "Reads " << toString(element->getDataType())
-       << " value: " << toString(element->getMetricValue()) << endl;
-  cout << string(offset, ' ') << "Writes " << toString(element->getDataType())
-       << " value type" << endl;
-  cout << endl;
-}
-
-void print(NonemptyFunctionPtr element, size_t offset) {
-  cout << string(offset, ' ') << "Executes " << toString(element->result_type)
-       << " call(" << toString(element->parameters) << ")" << endl;
-  cout << endl;
-}
-
-void print(NonemptyDeviceElementPtr element, size_t offset) {
-  cout << string(offset, ' ') << "Element name: " << element->getElementName()
-       << endl;
-  cout << string(offset, ' ') << "Element id: " << element->getElementId()
-       << endl;
-  cout << string(offset, ' ')
-       << "Described as: " << element->getElementDescription() << endl;
-
-  match(element->functionality,
-      [offset](NonemptyDeviceElementGroupPtr interface) {
-        print(interface, offset);
-      },
-      [offset](NonemptyMetricPtr interface) { print(interface, offset); },
-      [offset](
-          NonemptyWritableMetricPtr interface) { print(interface, offset); },
-      [offset](NonemptyFunctionPtr interface) { print(interface, offset); });
-}
-
-void print(const DevicePtr& device) {
-  cout << "Device name: " << device->getElementName() << endl;
-  cout << "Device id: " << device->getElementId() << endl;
-  cout << "Described as: " << device->getElementDescription() << endl;
-  cout << endl;
-  print(device->getDeviceElementGroup(), 3);
 }
