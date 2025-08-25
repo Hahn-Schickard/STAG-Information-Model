020b11e967c25b27610fdec3716aa6ae3ba8a4c4
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2025-08-25 16:56:28 +0200

    add MetaInfoMock implementation

--- includes/Mocks/MetaInfoMock.hpp
+++ includes/Mocks/MetaInfoMock.hpp
@@ -3,17 +3,32 @@
 #include "MetaInfo.hpp"
 
 #include <gmock/gmock.h>
+#include <optional>
 
 namespace Information_Model::testing {
 
+struct FullMetaInfo {
+  std::string name;
+  std::string description;
+};
+
 struct MetaInfoMock : virtual public MetaInfo {
   MetaInfoMock() = default;
+
+  MetaInfoMock(const std::string& id,
+      const std::optional<FullMetaInfo>& meta = std::nullopt);
 
   ~MetaInfoMock() override = default;
 
   MOCK_METHOD(std::string, id, (), (const final));
   MOCK_METHOD(std::string, name, (), (const final));
   MOCK_METHOD(std::string, description, (), (const final));
+
+private:
+  std::string id_;
+  std::optional<FullMetaInfo> meta_;
 };
+
+using MetaInfoMockPtr = std::shared_ptr<MetaInfoMock>;
 } // namespace Information_Model::testing
 #endif //__STAG_INFORMATION_MODEL_META_INFO_MOCK_HPP
