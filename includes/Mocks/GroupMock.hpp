e26c09510debf20192b5dbe00d08d41971ce9d82
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2025-09-02 17:37:27 +0200

    add GroupMock implementation

--- includes/Mocks/GroupMock.hpp
+++ includes/Mocks/GroupMock.hpp
@@ -8,12 +8,23 @@
 namespace Information_Model::testing {
 
 struct GroupMock : public Group {
-  GroupMock() = default;
+  explicit GroupMock(const std::string& id);
 
   ~GroupMock() override = default;
 
   MOCK_METHOD(Group::Elements, getElements, (), (const final));
   MOCK_METHOD(ElementPtr, getElement, (const std::string&), (const final));
+
+  std::string generateID();
+  void addDeviceElement(const ElementPtr& element);
+
+private:
+  ElementPtr _getElement(const std::string& ref_id);
+
+  std::unordered_map<std::string, ElementPtr> elements_;
+  std::unordered_map<std::string, GroupPtr> subgroups_;
+  size_t next_id_;
+  std::string id_;
 };
 
 using GroupMockPtr = std::shared_ptr<GroupMock>;
---
+++ sources/Mocks/GroupMock.cpp
@@ -0,0 +1,73 @@
+#include "GroupMock.hpp"
+#include "Element.hpp"
+
+namespace Information_Model::testing {
+using namespace std;
+using namespace ::testing;
+
+string GroupMock::generateID() {
+  string new_id = (id_.back() == ':' ? id_ : id_ + '.') + to_string(next_id_);
+  next_id_++;
+  return new_id;
+}
+
+GroupMock::GroupMock(const string& id) : id_(id) {}
+
+void GroupMock::addDeviceElement(const ElementPtr& element) {
+  if (!element) {
+    throw invalid_argument("Given element is empty");
+  }
+  if (element->id().compare(0, id_.length(), id_) != 0) {
+    throw invalid_argument("Given element is not part of this group");
+  }
+  if (element->id() == id_) {
+    throw invalid_argument("Given element has the same ID as this group");
+  }
+
+  auto sub_id = element->id().substr(id_.size() + 1);
+  if (!elements_.try_emplace(sub_id, element).second) {
+    throw logic_error(
+        "Element with id " + element->id() + " is already in this group");
+  }
+  if (element->type() == ElementType::Group) {
+    auto subgroup_id = sub_id.substr(0, sub_id.find('.'));
+    if (!subgroups_
+            .try_emplace(subgroup_id, dynamic_pointer_cast<Group>(element))
+            .second) {
+      throw logic_error("Subgroup with id " + subgroup_id + " for element " +
+          element->id() + " is already indexed");
+    }
+  }
+}
+
+ElementPtr GroupMock::_getElement(const string& ref_id) {
+  if (ref_id.compare(0, id_.length(), id_) != 0) {
+    // if referenced id does not start with this id, than the element is not in
+    // this group
+    throw ElementNotFound(ref_id);
+  }
+  if (ref_id == id_) {
+    throw IDPointsThisGroup(ref_id);
+  }
+
+  auto sub_id = ref_id.substr(id_.size() + 1);
+  auto group_marker = sub_id.find('.');
+  if (group_marker == string::npos) {
+    if (auto it = elements_.find(sub_id); it != elements_.end()) {
+      return it->second;
+    } else {
+      // no element with given id exists
+      throw ElementNotFound(ref_id);
+    }
+  } else {
+    auto subgroup_id = sub_id.substr(0, group_marker);
+    if (auto it = subgroups_.find(subgroup_id); it != subgroups_.end()) {
+      return it->second->getElement(ref_id);
+    } else {
+      // no subgroup containing given id exists
+      throw ElementNotFound(ref_id);
+    }
+  }
+}
+
+} // namespace Information_Model::testing
